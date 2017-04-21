d9d65d3602fde0e5780daf654a61a5875b99631c
yangfei <yfei89@126.com>
2017-04-21 23:07:42 +0800

    fix bugs

---
+++ app/components/productBox.js
@@ -0,0 +1,12 @@
+
+import React from 'react';
+let ProductBox = React.createClass({
+  render: function () {
+    return (
+      <div className="productBox">
+        hello!
+      </div>
+    );
+  }
+});
+export default ProductBox;
